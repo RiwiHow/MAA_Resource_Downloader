b4e346eba738b0e033223d194a7126dbc0620c80
RiwiHow <53964574+RiwiHow@users.noreply.github.com>
2024-10-25 11:32:01 +0800

    refactor: remove unnecessary 'os' module from cx_Freeze include list

--- src/maa_resource_downloader/setup.py
+++ src/maa_resource_downloader/setup.py
@@ -1,14 +1,13 @@
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
-    "packages": ["os", "extract", "read_path", "check_status", "notification"],
+    "includes": ["extract", "read_path", "check_status", "notification"],
     "include_files": ["config.json"],
-    "excludes": []
 }
 
 setup(
     name = "MAA_Resource_Downloader",
     version = "1.0",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("main.py", base="Win32GUI")]
+    executables = [Executable("main.py", base="gui")]
 )
