9993198cba395547e52a485a955622038e20c33b
RiwiHow <53964574+RiwiHow@users.noreply.github.com>
2024-10-28 21:49:56 +0800

    feat: avoid duplicate-notifications (2/2)

--- src/maa_resource_downloader/notification.py
+++ src/maa_resource_downloader/notification.py
@@ -2,14 +2,26 @@ from datetime import datetime
 from plyer import notification
 import json
 
+JSON_FILE = "config.json"
+
+def save_config(status):
+    with open(JSON_FILE, "w") as file:
+        json.dump(status, file, indent = 2)
+
 with open("config.json", "r") as file:
     config = json.load(file)
 
 def annihilation_notification():
+    if "annihilation_flag" not in config:
+        config["annihilation_flag"] = False
+        save_config(config)
+
     if datetime.now().weekday() == 6 and datetime.now().hour >= 12 and config["annihilation_notification"] and config["annihilation_flag"]:
         notification.notify(title='Annihilation Attention', message='Annihilation is completed or not?', timeout=10)
         config["annihilation_flag"] = False
+        save_config(config)
 
     if datetime.now().weekday() != 6:
         config["annihilation_flag"] = True
+        save_config(config)
 
